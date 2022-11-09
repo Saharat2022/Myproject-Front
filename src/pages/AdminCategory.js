290559a3e14b7f3cf65f04d391c4d13d49057017
Saharat2022 <saharat59010@gmail.com>
2022-11-10 00:45:09 +0700

    fix

--- src/pages/AdminCategory.js
+++ src/pages/AdminCategory.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Cards from "../component/firstpage/Cards";
+import React from 'react';
+import Cards from '../component/firstpage/Cards';
 
-import TabAdmin from "../component/TabAdmin";
-import { useAuth } from "../contexts/AuthContext";
+import TabAdmin from '../component/TabAdmin';
+import { useAuth } from '../contexts/AuthContext';
 
 function AdminCategory() {
   const { user } = useAuth();
@@ -10,7 +10,7 @@ function AdminCategory() {
     <div className="mt-[40px] mb-[40px]">
       <div className="w-9/12 mx-auto h-full flex justify-between gap-3">
         {user ? (
-          user.role !== "admin" ? (
+          user.role !== 'admin' ? (
             <div className="border-red-900 border-2 w-full rounded-md">
               <div className="w-full h-full flex justify-center">
                 <Cards />
@@ -19,7 +19,7 @@ function AdminCategory() {
           ) : (
             <>
               <TabAdmin />
-              <div className="border-red-900 border-2 w-3/4 p-10 rounded-md flex justify-center border-opacity-60 rounded-md">
+              <div className="border-red-900 border-2 w-3/4 p-10 flex justify-center border-opacity-60 rounded-md">
                 <div className="w-full h-full ">
                   <Cards />
                 </div>
